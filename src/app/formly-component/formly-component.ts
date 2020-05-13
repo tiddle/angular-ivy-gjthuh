e4cb83ba03de114c37cb6aeca5bca05abedf890b
tiddle <me@carlocruz.com>
2020-05-13 20:55:44 +1000

    2 files modified

--- src/app/formly-component/formly-component.ts
+++ src/app/formly-component/formly-component.ts
@@ -23,65 +23,7 @@ export class FormlyTestComponent {
         placeholder: 'Formly is terrific!',
         required: true,
       },
-    },
-    {
-      key: 'nested.story',
-      type: 'textarea',
-      templateOptions: {
-        label: 'Some sweet story',
-        placeholder: 'It allows you to build and maintain your forms with the ease of JavaScript :-)',
-        description: '',
-      },
-      expressionProperties: {
-        'templateOptions.focus': 'formState.awesomeIsForced',
-        'templateOptions.description': (model, formState) => {
-          if (formState.awesomeIsForced) {
-            return 'And look! This field magically got focus!';
-          }
-        },
-      },
-    },
-    {
-      key: 'awesome',
-      type: 'checkbox',
-      templateOptions: { label: '' },
-      expressionProperties: {
-        'templateOptions.disabled': 'formState.awesomeIsForced',
-        'templateOptions.label': (model, formState) => {
-          if (formState.awesomeIsForced) {
-            return 'Too bad, formly is really awesome...';
-          } else {
-            return 'Is formly totally awesome? (uncheck this and see what happens)';
-          }
-        },
-      },
-    },
-    {
-      key: 'whyNot',
-      type: 'textarea',
-      expressionProperties: {
-        'templateOptions.placeholder': (model, formState) => {
-          if (formState.awesomeIsForced) {
-            return 'Too bad... It really is awesome! Wasn\'t that cool?';
-          } else {
-            return 'Type in here... I dare you';
-          }
-        },
-        'templateOptions.disabled': 'formState.awesomeIsForced',
-      },
-      hideExpression: 'model.awesome',
-      templateOptions: {
-        label: 'Why Not?',
-        placeholder: 'Type in here... I dare you',
-      },
-    },
-    {
-      key: 'custom',
-      type: 'custom',
-      templateOptions: {
-        label: 'Custom inlined',
-      },
-    },
+    }
   ];
 
   submit() {
